18cecc5f983a04a6e1c2800a273ffa5b3c9db535
Musodzhon Zakirov <i.zulayho@gmail.com>
2021-11-06 17:53:13 +0500

    Добавьте файлы проекта.

---
+++ WebApplication/Context/QuotesDBContext.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication
+{
+    public class QuotesDBContext : DbContext
+    {
+        public QuotesDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Quote>().HasData
+                (
+                new Quote
+                {
+                    Id = Guid.NewGuid(),
+                    Text = "When a thing distrubs the peace of your hurt give it up",
+                    Author = "Pophet Muhammad (SAW)",
+                    InsertDate = DateTime.Now
+                },
+            new Quote
+            {
+                Id = Guid.NewGuid(),
+                Text = "In order to write about life first you must live it.",
+                Author = "Ernest Hemingway",
+                InsertDate = DateTime.Now
+            },
+            new Quote
+            {
+                Id = Guid.NewGuid(),
+                Text = "Genius is 1% talent and 99% percent hard work...",
+                Author = "Albert Einstein",
+                InsertDate = DateTime.Now
+            },
+            new Quote
+            {
+                Id = Guid.NewGuid(),
+                Text = "Money and success don’t change people; they merely amplify what is already there.",
+                Author = "Will Smith",
+                InsertDate = DateTime.Now
+            },
+
+            new Quote
+            {
+                Id = Guid.NewGuid(),
+                Text = "Memento mori",
+                Author = "stoicism",
+                InsertDate = DateTime.Now
+            }
+                );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data source=quotedb.db");
+        }
+
+        public DbSet<Quote> Quotes { get; set; }
+
+        public QuotesDBContext()
+        {
+        }
+
+    }
+}
---
+++ WebApplication/Migrations/20211106124935_Quotsend.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Migrations
+{
+    public partial class Quotsend : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quotes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Text = table.Column<string>(type: "TEXT", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", nullable: true),
+                    InsertDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotes", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Quotes",
+                columns: new[] { "Id", "Author", "InsertDate", "Text" },
+                values: new object[] { new Guid("add3f7db-1191-44a9-af14-4d6c43384771"), "Pophet Muhammad (SAW)", new DateTime(2021, 11, 6, 17, 49, 34, 602, DateTimeKind.Local).AddTicks(1309), "When a thing distrubs the peace of your hurt give it up" });
+
+            migrationBuilder.InsertData(
+                table: "Quotes",
+                columns: new[] { "Id", "Author", "InsertDate", "Text" },
+                values: new object[] { new Guid("e8ae0d48-69e6-4d85-80c1-5e4308c4cf45"), "Ernest Hemingway", new DateTime(2021, 11, 6, 17, 49, 34, 603, DateTimeKind.Local).AddTicks(5457), "In order to write about life first you must live it." });
+
+            migrationBuilder.InsertData(
+                table: "Quotes",
+                columns: new[] { "Id", "Author", "InsertDate", "Text" },
+                values: new object[] { new Guid("c00eac12-5046-428f-b619-9b15b578a2f7"), "Albert Einstein", new DateTime(2021, 11, 6, 17, 49, 34, 603, DateTimeKind.Local).AddTicks(5484), "Genius is 1% talent and 99% percent hard work..." });
+
+            migrationBuilder.InsertData(
+                table: "Quotes",
+                columns: new[] { "Id", "Author", "InsertDate", "Text" },
+                values: new object[] { new Guid("108eaa30-1f39-4e6c-8155-8802b018be03"), "Will Smith", new DateTime(2021, 11, 6, 17, 49, 34, 603, DateTimeKind.Local).AddTicks(5488), "Money and success don’t change people; they merely amplify what is already there." });
+
+            migrationBuilder.InsertData(
+                table: "Quotes",
+                columns: new[] { "Id", "Author", "InsertDate", "Text" },
+                values: new object[] { new Guid("a3b9d1d5-8f64-4eea-bffa-1e27b967b3c9"), "stoicism", new DateTime(2021, 11, 6, 17, 49, 34, 603, DateTimeKind.Local).AddTicks(5492), "Memento mori" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quotes");
+        }
+    }
+}
